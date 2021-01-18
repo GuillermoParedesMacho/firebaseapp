aeb95fb15af9fab5a63bb1fe92f6521f0533b319
Guillermo Paredes Macho <2c62b044cbc469601ae3bdade3f7c7b2db07f3ab>
2021-01-18 18:21:20 +0000

    updtate

---
+++ Pods/Target Support Files/Pods-firebaseapp-firebaseappUITests/Pods-firebaseapp-firebaseappUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_firebaseapp_firebaseappUITests : NSObject
+@end
+@implementation PodsDummy_Pods_firebaseapp_firebaseappUITests
+@end
--- firebaseapp/ViewController.swift
+++ firebaseapp/ViewController.swift
@@ -3,6 +3,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func crash(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
